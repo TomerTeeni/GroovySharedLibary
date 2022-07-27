7611dec8f8222456b4cd1fdbcf247fd9f468a312
Tomer Teeni <67914362+TomerTeeni@users.noreply.github.com>
2022-07-28 00:10:12 +0300

    ;

--- vars/sourceControl.groovy
+++ vars/sourceControl.groovy
@@ -1,6 +1,4 @@
-def call(Map config) {
-    if(env.BUILD_NUMBER % 2 == 0) {
-    if(env.BUILD_NUMBER.toInteger() % 2 == 0) {
-        return true
-    }
-    return false
+def call(Map config=[:]) {
+  checkout scm
+  sh "env | sort"
+}
