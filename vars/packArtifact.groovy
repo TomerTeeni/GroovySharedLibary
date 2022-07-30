1c7d2e98e4bc098d821489be11f75e41069e9ddb
Tomer Teeni <67914362+TomerTeeni@users.noreply.github.com>
2022-07-30 15:36:54 +0300

    cccc

--- vars/packArtifact.groovy
+++ vars/packArtifact.groovy
@@ -1,7 +1,8 @@
 def call(Map config = [:]) {
      echo "Pack ${env.BUILD_NUMBER} "
-     sh 'mkdir archive'
-     sh 'echo test > archive/test.txt'
-     zip zipFile: 'test.zip', archive: false, dir: 'archive'
-     archiveArtifacts artifacts: 'test.zip', fingerprint: true
+   //  sh 'rm archive'
+   //  sh 'mkdir archive'
+   //  sh 'echo test > archive/test.txt'
+     zip zipFile: "test_${env.BUILD_NUMBER}.zip", archive: false, dir: 'dist/'
+     archiveArtifacts artifacts: "test_${env.BUILD_NUMBER}.zip", fingerprint: true
 }
